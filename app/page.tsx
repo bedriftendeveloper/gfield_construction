08b72ba6e948855cf8bd98cf603725d1e063f08b
kwabena369 <bismarkboampong6@gmail.com>
2025-10-02 15:25:30 +0000

    update:first-state

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import CTASection from './components/CTASection';
+import HeroSection from './components/HeroSection';
+import ProjectsSection from './components/ProjectsSection';
+import ServicesSection from './components/ServicesSection';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <HeroSection />
+      <ServicesSection />
+            <ProjectsSection />
+            <CTASection />
+
+    </main>
+  );
+}
